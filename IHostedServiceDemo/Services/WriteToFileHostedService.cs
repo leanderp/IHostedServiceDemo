39a9d40fc7064f926dd7dc5829c3b4db41a12c4b
Leanderp <leanderperez15@gmail.com>
2021-03-02 19:53:18 -0400

    Agregar archivos de proyecto.

---
+++ IHostedServiceDemo/Services/WriteToFileHostedService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IHostedServiceDemo.Services
+{
+    public class WriteToFileHostedService : IHostedService
+    {
+        private readonly IHostingEnvironment environment;
+        private readonly string fileName = "File1.txt";
+
+        public WriteToFileHostedService(IHostingEnvironment environment)
+        {
+            this.environment = environment;
+        }
+        
+        Task IHostedService.StartAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService: Process Started");
+            return Task.CompletedTask;
+        }
+
+        Task IHostedService.StopAsync(CancellationToken cancellationToken)
+        {
+            WriteToFile("WriteToFileHostedService: Process Stopped");
+            return Task.CompletedTask;
+        }
+
+        private void WriteToFile(string message)
+        {
+            var path = $@"{environment.ContentRootPath}\wwwroot\{fileName}";
+            using (StreamWriter writer = new StreamWriter(path,append: true))
+            {
+                writer.WriteLine(message);
+            }
+        }
+    }
+}
